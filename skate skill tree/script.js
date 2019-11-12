681a16e439bd236598b4f14189c04bbe0dddca84
Snawley <johnbutler10@outlook.com>
2019-11-12 18:27:12 -0500

    skill tree up on child page as inline SVGs

---
+++ skate skill tree/script.js
@@ -0,0 +1,17 @@
+var isBiggerFunction = function(a, b){
+  var result;
+  a>b ? result = true : result = false;
+  return result;
+}
+var result = isBiggerFunction(2,1);
+//document.body.innerHTML = "<h1>result!: " + result + "</h1>";
+
+function showArea(event){
+  alert("you have clicked me");
+}
+
+document.addEventListener('click', function (event) {
+	if (!event.target.matches('.tricks')){
+    alert("you have clicked me");
+  } return;
+}, false);
