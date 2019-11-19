7ff99bf395f69334fe1270cd8f2816088d6426fb
Snawley <johnbutler10@outlook.com>
2019-11-18 19:30:52 -0500

    got exp button and version buttons working

--- skate skill tree/skate_skills_script.js
+++ skate skill tree/skate_skills_script.js
@@ -26,37 +26,43 @@ function updateAvailable(id) {
 
 function learnTrick(event){
   // target will always be the path
-  event.target.parentElement.style.opacity = "1";
-  event.target.setAttribute("class", "trick_shape_path_learned");
-  updateAvailable(event.target.parentElement.id);
+  if(exp >= 5) {
+    event.target.setAttribute("class", "trick_shape_path_learned");
+    updateAvailable(event.target.parentElement.id);
+    exp -= 5;
+    document.getElementById("myExp").innerHTML = exp + " mL of blood sacrificed to Skatan";
+  }
+  else {alert("your exp (" + exp + ") is not enough to learn this trick");}
 }
 
-function trick(id, prerequisites) {
+function trick(id, prerequisites, expRequired) {
   this.id = id;
   this.prerequisites = prerequisites;
+  this.exp = expRequired;
 }
 
-var doubleHeel = new trick("doubleHeel", ["heel"]);
-var inwardDouble = new trick("inwardDouble", ["inward"]);
-var _360Inward = new trick("360Inward", ["inward"]);
-var impossible = new trick("impossible", ["popShuv"]);
-var tre = new trick("tre", ["varial"]);
-var nightmare = new trick("nightmare", ["varial"]);
-var doubleFlip = new trick("doubleFlip", ["kickflip"]);
-var doubleHard = new trick("doubleHard", ["hardflip"]);
-var _360Hard = new trick("360Hard", ["hardflip"]);
-var _360FrontShuv = new trick("360FrontShuv", ["frontShuv"]);
-var lazer = new trick("lazer", ["varialHeel"]);
-var daydream = new trick("daydream", ["varialHeel"]);
+var exp = 0;
+var doubleHeel = new trick("doubleHeel", ["heel"], 18);
+var inwardDouble = new trick("inwardDouble", ["doubleHeel", "inward"], 75);
+var _360Inward = new trick("360Inward", ["inward"], 75);
+var impossible = new trick("impossible", ["popShuv"], 21);
+var tre = new trick("tre", ["varial"], 20);
+var nightmare = new trick("nightmare", ["doubleFlip", "varial"], 23);
+var doubleFlip = new trick("doubleFlip", ["kickflip"], 14);
+var doubleHard = new trick("doubleHard", ["doubleFlip", "hardflip"], 44);
+var _360Hard = new trick("360Hard", ["hardflip"], 46);
+var _360FrontShuv = new trick("360FrontShuv", ["frontShuv"], 25);
+var lazer = new trick("lazer", ["varialHeel"], 30);
+var daydream = new trick("daydream", ["doubleHeel", "varialHeel"], 35);
 
-var heel = new trick("heel", ["ollie"]);
-var inward = new trick("inward", ["heel", "popShuv"]);
-var popShuv = new trick("popShuv", ["ollie"]);
-var varial = new trick("varial", ["kickflip", "popShuv"]);
-var kickflip = new trick("kickflip", ["ollie"]);
-var hardflip = new trick("hardflip", ["kickflip", "frontShuv"]);
-var frontShuv = new trick("frontShuv", ["ollie"]);
-var varialHeel = new trick("varialHeel", ["heel", "frontShuv"]);
+var heel = new trick("heel", ["ollie"], 9);
+var inward = new trick("inward", ["heel", "popShuv"], 28);
+var popShuv = new trick("popShuv", ["ollie"], 1);
+var varial = new trick("varial", ["kickflip", "popShuv"], 11);
+var kickflip = new trick("kickflip", ["ollie"], 7);
+var hardflip = new trick("hardflip", ["kickflip", "frontShuv"], 22);
+var frontShuv = new trick("frontShuv", ["ollie"], 3);
+var varialHeel = new trick("varialHeel", ["heel", "frontShuv"], 19);
 
 var tricks = [doubleHeel, inwardDouble, _360Inward, impossible, tre,
               nightmare, doubleFlip, doubleHard, _360Hard, _360FrontShuv,
@@ -64,9 +70,29 @@ var tricks = [doubleHeel, inwardDouble, _360Inward, impossible, tre,
               hardflip, frontShuv, varialHeel];
 
 var trick_paths = document.getElementsByClassName("trick_shape_path"); // all 21 trick shape paths
-for(var j = 0; j < trick_paths.length; j++){
-
-  trick_paths[j].addEventListener("click", learnTrick, false);
+for(var i = 0; i < trick_paths.length; i++){
+  trick_paths[i].addEventListener("click", learnTrick, false);
 }
 document.getElementById("ollie").getElementsByClassName("trick_shape_path")[0].className.baseVal = "trick_shape_path_available";
 //.setAttribute(("class"), "trick_shape_path_available");
+
+function requireDoubleFlips(){
+  inwardDouble.prerequisites = ["doubleHeel", "inward"];
+  nightmare.prerequisites = ["doubleFlip", "varial"];
+  doubleHard.prerequisites = ["doubleFlip", "hardflip"];
+  daydream.prerequisites = ["doubleHeel", "varialHeel"];
+}
+function noDoubleFlips(){
+  inwardDouble.prerequisites = ["inward"];
+  nightmare.prerequisites = ["varial"];
+  doubleHard.prerequisites = ["hardflip"];
+  daydream.prerequisites = ["varialHeel"];
+}
+document.getElementById("v2").addEventListener("click", requireDoubleFlips, false);
+document.getElementById("v1").addEventListener("click", noDoubleFlips, false);
+
+function gainExp(){
+  exp += 5;
+  document.getElementById("myExp").innerHTML = exp + " mL of blood sacrificed to Skatan";
+}
+document.getElementById("gain").addEventListener("click", gainExp, false);
