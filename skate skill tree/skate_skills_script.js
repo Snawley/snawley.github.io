8a2fd39bb3244e08fcc9c518f979e4c52e008a01
Snawley <johnbutler10@outlook.com>
2019-12-06 23:58:15 -0500

    level counter with resizing of fraction's letter-spacing

--- skate skill tree/skate_skills_script.js
+++ skate skill tree/skate_skills_script.js
@@ -5,11 +5,12 @@
   */
 
 function hideHoverText(event) {
+  document.querySelector("#reqsElement").style.visibility = "hidden";
   let topMessage = document.querySelector("h1.message");
   topMessage.innerHTML = "SKATE UP!"; // top message default
-  topMessage.style.color = "#32323c"; // default trick fill
+  topMessage.style.color = "#32323c"; // default trick fill (dark grey)
   document.querySelector("#expGain").innerHTML = ""; // clear the exp gain
-  document.querySelector("h3.reqs").innerHTML = ""; // clear the reqs heading
+  document.querySelector("h2.reqs").innerHTML = ""; // clear the reqs heading
   document.querySelector("ul.reqs").innerHTML = ""; // clear the unordered list
   if(event.target.parentElement.querySelector(".trick_text_path_available") != null) {
     event.target.parentElement.querySelector(".trick_text_path_available").style.removeProperty("fill");
@@ -19,9 +20,11 @@ function hideHoverText(event) {
   }
 }
 
-var unmetReqColour = "#FFb3b3";
-var metReqColour = "#66b366";
-var defaultLineHeight = "1.5em";
+var unmetReqColour = "#FFb3b3"; //pink
+var metReqColour = "#66b366"; // green
+var blue = "#3A3AC1"; // blue
+var lightBlue = "#c8c8f0"
+var defaultLineHeight = "2.5rem";
 function getColouredElement(reqText, satisfied, lineHeight = defaultLineHeight) {
   let req = document.createElement("li");
   req.appendChild(document.createTextNode(reqText));
@@ -32,9 +35,9 @@ function getColouredElement(reqText, satisfied, lineHeight = defaultLineHeight) 
 
 function appendReqsToUL(trick) {
   var reqsUL = document.querySelector("ul.reqs");
-  // first append the exp requirement
-  let expReqText = trick.expReq + " exp. (" + exp + "/" + trick.expReq + ")";
-  reqsUL.appendChild(getColouredElement(expReqText, (exp >= trick.expReq)));
+  // we no longer append the exp requirement
+  /*let expReqText = trick.expReq + " exp. (" + exp + "/" + trick.expReq + ")";
+  reqsUL.appendChild(getColouredElement(expReqText, (exp >= trick.expReq)));*/
   // now apeend the trick prerequisites
   if(trick.numPreReqs >= 1) {
     let preReq1 = trick.preReqs[0];
@@ -67,14 +70,15 @@ function startsWithVowel(id) {
   return "AEIOU".includes(firstChar);
 }
 function showHoverText(trick, event) {
+  let reqsElement = document.querySelector("#reqsElement");
   let topMessage = document.querySelector("h1.message")
-  let reqsHeading = document.querySelector("h3.reqs");
+  let reqsHeading = reqsElement.querySelector("h2.reqs");
   if(event.target.classList.contains("trick_shape_path_learned")) {
     // trick has been learned
-    event.target.parentElement.querySelector(".trick_text_path_learned").style.fill = "#c8c8f0";
+    event.target.parentElement.querySelector(".trick_text_path_learned").style.fill = lightBlue;
     let an = startsWithVowel(trick.id) ? "an" : "a";
     topMessage.innerHTML = "Click to do " + an + " " + trick.id + "!";
-    topMessage.style.color = "#3A3AC1";
+    topMessage.style.color = blue;
     let expGain = document.querySelector("#expGain");
     expGain.innerHTML = "( +" + trick.expGain + " exp. )";
     return;
@@ -86,6 +90,9 @@ function showHoverText(trick, event) {
     topMessage.style.color = metReqColour;
     reqsHeading.innerHTML = "You can learn " + trick.id + "!";
     reqsHeading.style.color = metReqColour;
+    reqsElement.style.visibility = "visible";
+    reqsElement.style.borderTop = "2px solid " + metReqColour;
+    reqsElement.style.borderLeft = "2px solid " + metReqColour;
   }
   else {
     // trick is not available yet
@@ -94,8 +101,68 @@ function showHoverText(trick, event) {
     topMessage.style.color = unmetReqColour;
     reqsHeading.innerHTML = "Requirements to learn " + trick.id + ":";
     reqsHeading.style.color = unmetReqColour;
+    reqsElement.style.visibility = "visible";
+    reqsElement.style.borderTop = "2px solid " + unmetReqColour;
+    reqsElement.style.borderLeft = "2px solid " + unmetReqColour;
   }
   appendReqsToUL(trick);
+}
+
+function sizeFraction(id) {
+  let len = document.querySelector(id).innerHTML.length;
+  console.log(len);
+  if(len <= 3) document.querySelector(id).style.letterSpacing = "3px"; // Eg: 1/3
+  else if(len == 4) document.querySelector(id).style.letterSpacing = "2px"; // Eg: 1/10
+  else if(len == 5) document.querySelector(id).style.letterSpacing = "1px"; // Eg: 10/100
+  else if(len == 6) document.querySelector(id).style.letterSpacing = "0px"; // Eg: 10/100
+  else if(len >= 7) { // Eg: 100/200
+    document.querySelector(id).style.letterSpacing = "-1px";
+    document.querySelector(id).style.fontSize = "0.8em";
+  }
+}
+function updateCounters(){
+  if(currentLevel == levels.length - 1) { // we are the highest level
+    // so remove the exp bar and trick bar from the page
+    document.querySelector("#bars").innerHTML = "";
+    return;
+  }
+  let nextLevel = levels[currentLevel + 1];
+  document.querySelector("#expCount").innerHTML = exp + "/" + nextLevel.expReq;
+  sizeFraction("#expCount");
+  document.querySelector("#trickCount").innerHTML = tricksLearned + "/" + nextLevel.numTricks;
+  sizeFraction("#trickCount");
+
+  if(exp >= nextLevel.expReq) { // exp requirement has been fullfilled
+    document.querySelector("#expBar").style.width = "100%";
+    document.querySelector("#expCount").style.color = blue;
+  }
+  else { // exp requiement is a fraction
+    document.querySelector("#expBar").style.width = (exp * 100.0 / nextLevel.expReq) + "%";
+    document.querySelector("#expCount").style.color = lightBlue;
+  }
+  if(tricksLearned >= nextLevel.numTricks) { // tricks learned requirement has been fullfilled
+    document.querySelector("#tricksBar").style.width = "100%";
+    document.querySelector("#trickCount").style.color = metReqColour;
+  }
+  else { //trick learned is a fraction
+    //let tricksBarWidth = ((tricksLearned - levels[currentLevel].numTricks) * 100.0) / (nextLevel.numTricks - levels[currentLevel].numTricks);
+    let tricksBarWidth = (tricksLearned * 100.0) / nextLevel.numTricks;
+    document.querySelector("#tricksBar").style.width = tricksBarWidth + "%";
+    document.querySelector("#trickCount").style.color = unmetReqColour;
+  }
+}
+
+function updateLevel(){
+  // if the exp and tricksLearned requirement for the next level is met, go up a level!
+  if(currentLevel == levels.length - 1) {alert("you are a winner!");return;} // we are at the highest level
+  let nextLevel = levels[currentLevel + 1];
+  if(tricksLearned >= nextLevel.numTricks && exp >= nextLevel.expReq) { // go up a level!
+    currentLevel++;
+    document.querySelector("h2.level").innerHTML = "You are " + nextLevel.prefix + ":";
+    document.querySelector("h3.level").innerHTML = nextLevel.title;
+    exp -= nextLevel.expReq;
+  }
+  updateCounters();
 }
 
 function isVisible(trick) {
@@ -168,12 +235,13 @@ function updateAvailable(trick) {
 }
 
 function performTrick(trick, event){
-  exp += trick.expGain;
+  exp += trick.expGain; // gain exp based on which trick was performed
   trick.totalNumberDone++;
   trick.availableCnt++;
   for (let t in tricks) {
     updateAvailable(tricks[t]); // only updates visible tricks, and not learned tricks
   }
+  updateLevel();
 }
 
 function decreaseAvailableCntsInPreReqs(trick) {
@@ -201,16 +269,16 @@ function learnTrick(trick, event){ // learnTrick is only called on available tri
   hideHoverText(event);
   showHoverText(trick, event);
   trick.totalNumberDone = 1;
-  trick.availableCnt = 1;
+  trick.availableCnt = 0;
   decreaseAvailableCntsInPreReqs(trick);
   for(let t in tricks) {
     if(tricks[t].totalNumberDone == 0) {
-      tricks[t].expReq += expRamp; // increases all the unlearned tricks' expReq
       updateVisible(tricks[t]); // only changes hidden svgs to visible
       updateAvailable(tricks[t]); // only updates visible tricks, and not learned tricks
     }
   }
-  expRamp += 2; // learning tricks takes a linearly growing amount of exp
+  tricksLearned++; // we have learned a trick!
+  updateLevel();
 }
 
 function preReq(id, cnt, altId, altCnt) {
@@ -223,7 +291,7 @@ function trick(id, preReqs, expRequired, expGained) {
   this.id = id;
   this.preReqs = preReqs;
   this.numPreReqs = preReqs.length; // will be 1 or 2 for all tricks besides ollie
-  this.expReq = expRequired;
+  this.expReq = 0; // no longer have an expReq
   this.expGain = expGained;
   this.totalNumberDone = 0; // 0 have been done initially
   this.availableCnt = 0; // 0 have been done initially
@@ -257,9 +325,21 @@ tricks.varialHeel = new trick("varialHeel", [new preReq("heel", 4), new preReq("
 tricks.ollie = new trick("ollie", [], 0, 1)
 
 var exp = 0; //starting amount of experience
-var expRamp = 0; // how much we increase the expReq of tricks each time a trick is learned
-//var levels = {};
+var tricksLearned = 0; // starting number of tricks that have been learned
+var currentLevel = -1; // initialize the starting level by calling updateLevel
+
+function level(title, numTricks, expReq, prefix="a") {
+  this.prefix = prefix;
+  this.title = title;
+  this.numTricks = numTricks;
+  this.expReq = expReq;
+}
+var levels = [new level("poser", 0, 0), new level("snake", 1, 2), new level("rat on a skateboard", 2, 5), new level("grom", 4, 9),
+              new level("park shark", 5, 14), new level("skate rat", 7, 20), new level("shredder", 8, 27), new level("street rat", 10, 35),
+              new level("gnarly dude", 11, 44), new level("shop skater", 13, 54), new level("local legend", 14, 65), new level("Am", 16, 77, "an"),
+              new level("gnarly shredder", 17, 90), new level("Pro", 19, 104), new level("unbeatable at SKATE", 21, 120, "")];
 // add an event listener to ollie because it is available
 // don't show preReqs on hover for ollie
 updateVisible(tricks.ollie);
 updateAvailable(tricks.ollie); // we can do this by calling update avaialable
+updateLevel();
